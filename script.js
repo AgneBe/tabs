306e17d869a6a489dead18a8be35f4caa6e0055f
AgneBe <156712872+AgneBe@users.noreply.github.com>
2024-03-06 19:42:33 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+const tabs = document.querySelectorAll(".tab");
+const buttons = document.querySelectorAll("button");
+
+for (let i = 0; i < tabs.length; i++) {
+  tabs[i].style.display = "none";
+}
+
+function showTab(e, id) {
+  for (let i = 0; i < tabs.length; i++) {
+    tabs[i].style.display = "none";
+    tabs[i].classList.remove("active");
+  }
+
+  document.getElementById(id).style.display = "block";
+
+  setTimeout(() => {
+    document.getElementById(id).classList.add("active");
+  }, 0);
+
+  for (let i = 0; i < buttons.length; i++) {
+    buttons[i].classList.remove("selected-button");
+  }
+  
+  e.currentTarget.classList.add("selected-button");
+}
